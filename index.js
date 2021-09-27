f6a8266aeb2ca48b7b50e46f534e2bf49630b4cd
Jeni Dang <jenipdang@yahoo.com>
2021-09-26 21:49:05 -0700

    commit tree 50d28f3813553f247e0e1f935d083ab611b92135

---
+++ index.js
@@ -0,0 +1,16 @@
+let num1 = 100
+let num2 = 2000
+const divide = function () {
+    return num2 / num1;
+}
+divide();
+
+
+const square = x => {
+    return x * x;
+}
+
+const add = (parameter1, parameter2) => {
+ return parameter1 + parameter2;
+}
+
